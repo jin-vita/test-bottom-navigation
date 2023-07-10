9fb52ebe5983d255f5d6745099500881c6a81305
jin-vita <jhj9144@gmail.com>
2023-07-10 13:44:34 +0900

    feat connect fragments through navigation component

--- app/src/main/java/com/jinvita/testbottomnavigation/MainActivity.kt
+++ app/src/main/java/com/jinvita/testbottomnavigation/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.jinvita.testbottomnavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jinvita.testbottomnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navController by lazy { (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
